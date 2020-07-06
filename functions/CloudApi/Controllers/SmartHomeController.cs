7f5a024e460b1be45dc727bdf62c960442347876
Raul Tomita <raul.tomita@essensys.ro>
2020-07-06 22:41:02 +0300

    added cosmos and redis dependencies

---
+++ functions/CloudApi/Controllers/SmartHomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace CloudApi.Controllers
+{
+    public class SmartHomeController : Controller
+    {
+        public IActionResult Sync()
+        {
+            return Ok();
+        }
+
+        public IActionResult Query()
+        {
+            return Ok();
+        }
+    }
+}
